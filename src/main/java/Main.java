18f21fbe3526971db47671089cc525dde57d216d
taqu13579 <92580230+taqu13579@users.noreply.github.com>
2022-03-19 17:41:46 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    static Map<String, Class> types = new HashMap<>();
+
+    public static void main(String[] args) {
+
+    }
+}
